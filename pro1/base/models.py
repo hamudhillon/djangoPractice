969576dfea49c0440bb17f1833fe851f63f520bc
Harman <hamu.dhillon@gmail.com>
2025-08-05 12:41:13 +0530

    Models

--- pro1/base/models.py
+++ pro1/base/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class emp(models.Model):
+    name=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+    address=models.TextField()
+    department=models.CharField(max_length=50)
