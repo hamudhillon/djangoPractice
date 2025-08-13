e131e937efc24e99569406b889e0e64651617d42
Harman <hamu.dhillon@gmail.com>
2025-08-13 12:46:24 +0530

    AddPostFromHtml

--- pro1/blog/views.py
+++ pro1/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from datetime import datetime
 
 
 # Create your views here.
@@ -12,5 +13,39 @@ def blogSingle(request):
     return render(request,'singleBlog.html')
 
 def addPost(request):
+    print('aaaaasasdasddas')
+    tags=tag.objects.all()
+    categories=category.objects.all()
+    context={
+        'tags':tags,
+        'categories':categories
+    }
+    if request.method=='POST':
+        title=request.POST['title']
+        ftags=request.POST['tags']
+        desc=request.POST['desc']
+        cat=request.POST['cat']
+        image=request.FILES['image']
+        catOB=category.objects.get(id=cat)
 
-    return render(request,'addBlog.html')
+        allTags=[]
+        if ',' in ftags: 
+            for t in ftags.split(','):
+                mytags=tag.objects.get_or_create(name=t.lower())
+                print(mytags)
+                if mytags:
+                    allTags.append(mytags[0].id)
+        postOB=blog.objects.create(
+        title=title,
+        desc=desc,
+        category=catOB,
+        image=image,
+        created_at=datetime.now()
+        )
+       
+        postOB.tags.set(allTags)
+        postOB.save()
+
+            
+        
+    return render(request,'addBlog.html',context)
