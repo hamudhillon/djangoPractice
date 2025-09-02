4b23ed85b30f3d01b16eb69959103de92f8198e0
Harman <hamu.dhillon@gmail.com>
2025-09-02 11:30:35 +0530

    restApp

---
+++ myapipro/restApiApp/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path,include
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register(r'students',StudentViewSet)
+
+
+
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
